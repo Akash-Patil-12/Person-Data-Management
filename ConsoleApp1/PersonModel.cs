6b4cc80dff59c0ab23232f9cc25bbfe220c00d17
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-19 23:09:54 +0530

    [Akash] Add : Implemented Welcome message

---
+++ ConsoleApp1/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class PersonModel
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Age { get; set; }       
+    }
+}
